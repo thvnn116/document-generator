92c61436bd093e8fa8a0b5743b0c78c68cf339c1
thvnn116 <th.vnn116@gmail.com>
2026-03-05 14:53:46 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:"40px", fontFamily:"Arial"}}>
+      <h1>Document Generator</h1>
+      <p>Hệ thống tạo văn bản tự động</p>
+    </div>
+  )
+}
