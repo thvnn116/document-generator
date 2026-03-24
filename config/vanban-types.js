cf45c96e1e942add118ec5868d4d6b734cd12ad5
thvnn116 <th.vnn116@gmail.com>
2026-03-24 15:00:40 +0700

    Update vanban-types.js

--- config/vanban-types.js
+++ config/vanban-types.js
@@ -3,7 +3,7 @@ export const vanbanTypes = [
   {
     code: "PLHD",
     name: "Phụ lục hợp đồng lao động",
-    templateFile: "phuluc-hopdong.docx",
+    templateFile: "/templates/phuluc-hopdong.docx",
     fields: [
       "MS_HDLD", "HO_TEN", "NGAY_SINH", "GIOI_TINH", "NGHE_NGHIEP",
       "BO_PHAN", "MS_NV", "DC_THUONG_TRU", "SO_CMND", "NGAY_CAP",
@@ -13,7 +13,7 @@ export const vanbanTypes = [
   {
     code: "TMNV",
     name: "Thư mời nhận việc",
-    templateFile: "thumoi-nhanviec.docx",
+    templateFile: "/templates/thumoi-nhanviec.docx",
     fields: [
       "MS_HD", "HO_TEN", "NGAY_SINH", "DIEN_THOAI", "DC_THUONG_TRU",
       "VI_TRI_CV", "DC_CTY", "NGAY_NHAN_VIEC", "MUC_LUONG"
